dad66a9bb7174455ec8030d08306a4b1dbae04bd
Maria Anson <mariaansona@gmail.com>
2024-02-29 14:49:28 -0500

    Delete Linux_Exam directory

--- Linux_Exam/task1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/user/catkin_ws/src/linux_exam
-mkdir -p /home/user/catkin_ws/src/linux_exam/this/is/my/linux/exam
-cd /home/user/catkin_ws/src/linux_exam/this/is/my/linux/exam
-touch my_file.py
-
-echo This bash script has finished!
--- Linux_Exam/task3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/user/catkin_ws/src/linux_exam/this/is/my/linux/exam
-rm /home/user/catkin_ws/src/linux_exam/this/is/my/linux/exam/*
-touch exam{1..3}.py
-chmod 754 exam1.py
-chmod 501 exam2.py
-chmod 241 exam3.py
